75ac673601c0968f9077ee4d2149060edaf8211b
Juan Carlos <juan.oliveira@rarolabs.com.br>
2023-10-20 14:40:34 -0300

    altera chamada para axios

--- src/store/UserAuth/actions.ts
+++ src/store/UserAuth/actions.ts
@@ -1,41 +1,64 @@
 import { Commit } from 'vuex' // commit interface for typescript
 import router from '@/router'
+import axios from 'axios'
 
 import { UserType } from '@/store/UserAuth/types'
 
 export default {
     login({ commit }: { commit: Commit }, user: UserType) {
-
         const UserLoginDto = {
             Email: user.email,
-            Senha: user.password
-        };
+            Senha: user.password,
+        }
+        const headers = {
+            'Content-Type': 'application/json',
+        }
 
-        fetch('apimongodb.azurewebsites.net/Auth/login', {
-            method: 'POST',
-            body: JSON.stringify(UserLoginDto),
-            headers: { 'Content-Type': 'application/json' }
-        })
-            .then((response) => {
-                if (!response.ok) {
-                    console.log(response);
-                    throw new Error('Erro na solicitação de login')
-                }
-                return response.json()
+        axios
+            .post('https://localhost:5000/Auth/login', UserLoginDto, {
+                headers,
             })
-            .then((data) => {
-               
-                if (data.token) {
-                    console.log('200')
-                    commit('login', user.email)
-                    localStorage.setItem('email',user.email)
-                    localStorage.setItem('name',user.name)
-                    router.push('/welcome')
+            .then((response) => {
+                console.log(response);
+                if (response.status == 200) {
+                    if (response.data.token) {
+                        console.log('200');
+                        commit('login', user.email);
+                        localStorage.setItem('email', user.email);
+                        localStorage.setItem('name', user.name);
+                        router.push('/welcome');
+                    }
                 }
             })
             .catch((error) => {
-                console.error(error)
-            });
+                // Lidar com erros
+                console.error('Erro na requisição:', error)
+            })
+
+        // fetch('apimongodb.azurewebsites.net/Auth/login', {
+        //     method: 'POST',
+        //     body: JSON.stringify(UserLoginDto),
+        //     headers: { 'Content-Type': 'application/json' },
+        // })
+        //     .then((response) => {
+        //         if (!response.ok) {
+        //             console.log(response)
+        //             throw new Error('Erro na solicitação de login')
+        //         }
+        //         return response.json()
+        //     })
+        //     .then((data) => {
+        //         if (data.token) {
+        //             console.log('200')
+        //             commit('login', user.email)
+        //             localStorage.setItem('email', user.email)
+        //             localStorage.setItem('name', user.name)
+        //             router.push('/welcome')
+        //         }
+        //     })
+        //     .catch((error) => {
+        //         console.error(error)
+        //     })
 
         // const userIndex = registeredUsers.findIndex(
         //     (usersData) =>
